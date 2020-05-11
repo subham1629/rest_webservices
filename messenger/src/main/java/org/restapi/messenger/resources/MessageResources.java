8db64dd6bd449f2cef2fb1622c0bfd726c78afe2
Subham Sen <ananda.glmail@gmail.com>
2020-05-11 12:27:44 +0530

    "put and delete"

--- messenger/src/main/java/org/restapi/messenger/resources/MessageResources.java
+++ messenger/src/main/java/org/restapi/messenger/resources/MessageResources.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -31,6 +33,22 @@ public class MessageResources{
 	@Produces(MediaType.APPLICATION_JSON)
 	public Message postMessage(Message message) {
 		return messages.addMessage(message);
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("messageId") long id, Message message) {
+		message.setId(id);
+		return messages.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void deleteMessage(@PathParam("messageId") long id) {
+		messages.deleteMessage(id);
 	}
 	
 	@GET
